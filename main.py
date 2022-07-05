3c01d276f83f686573d97f028138e47eb0cd9b55
Rahyulty <85312026+Rahyulty@users.noreply.github.com>
2022-07-05 18:53:53 -0400

    Class 2

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import pygame
  
 pygame.init()
 
-# Game Variable
+# COnfig Game Variable
 Run_Game = True # False if you dont wan it to run 
+
 
 # Setting up our screen
 World_X = 800
@@ -15,11 +16,21 @@ Clock = pygame.time.Clock()
 Color_Blue = (25, 25, 200)
 Color_Black = (23, 23, 23)
 Color_White = (254, 254, 254)
-
+Color_Green = (86, 125, 70)
 
 The_World = pygame.display.set_mode([World_X, World_Y])
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.width = 48
+        self.height = 60
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(Color_White)
+        self.rect = self.image.get_rect()
+
 while Run_Game:
-    The_World.fill(Color_Blue)
+    The_World.fill(Color_Green)
     pygame.display.flip()
     Clock.tick(FPS)
